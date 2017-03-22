1a49460c2a9f80e85f65ea6789b2aba5d1aef9b8
Ams <adriansims62781@gmail.com>
2017-03-22 23:29:55 +0000

    Initial Commit

---
+++ ListVsExtensions/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ListVsExtensions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {   
+            try
+            {
+                var config = new VS2017Info.Vs2017SetupConfig();
+                foreach (var instance in config.VSInstances)
+                {
+                    Console.WriteLine($"{instance.Id}");
+                    var vsPath = VS2017Info.VS2017AppData.GetLocalAppDataPath(instance.Version, instance.Id);
+                    Console.WriteLine($"local appdata path is {vsPath}");
+
+                    var extensions = VS2017Info.VS2017AppData.GetInstalledExtensions(instance.Version, instance.Id);
+                    foreach (var ext in extensions)
+                    {
+                        Console.WriteLine($"{ext.Key} {ext.Value}");
+                    }
+
+
+                    var hivefile = VS2017Info.VS2017AppData.GetPrivateRegFilename(instance.Version, instance.Id);
+                    var hKey = RegistryNativeMethods.RegLoadAppKey(hivefile);
+
+                    var keyVersion = VS2017Info.VS2017AppData.GetVersionForRegistryKey(instance.Version, instance.Id);
+
+                    var keypath = $@"Software\Microsoft\VisualStudio\{keyVersion}\ExtensionManager\EnabledExtensions";
+                    using (var safeRegistryHandle = new SafeRegistryHandle(new IntPtr(hKey), true))
+                    using (var appKey = RegistryKey.FromHandle(safeRegistryHandle))
+                    using (var extensionsKey = appKey.OpenSubKey(keypath, true))
+                    {
+                        if (extensionsKey == null)
+                        {
+                            throw new ApplicationException("Specified file is not a well-formed software registry hive: " + hivefile);
+                        }
+
+                        //var extensions = extensionsKey.GetValueNames().Select(x=>x.Split(',').FirstOrDefault());
+                        //foreach (var extGuid in extensions)
+                        //{
+                        //    var value = extensionsKey.GetValue(extGuid);
+                        //    Console.WriteLine($"extension:{extGuid}: {value}");
+                        //}
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ListVsExtensions/VSInstanceInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace ListVsExtensions
+{
+    class VSInstanceInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string DispName { get; set; }
+        public string Description { get; set; }
+        public string ResPath { get; set; }
+        public string EnginePath { get; set; }
+        public string InstalledPath { get; set; }
+        public string ProductPath { get; set; }
+    }
+}
